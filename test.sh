6b22dbc79ee75b5c6250878b9ade45a1635a0a4b
Nikolaos Kavvadias <kavvadias@silexica.com>
2017-01-08 15:17:50 +0100

    But also no valgrind

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ EXE=.exe
 # Test unsigned and signed multiplications
 for mulu in "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "23" "37" "43" "111" "255"
 do
-  valgrind ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -nac
-  valgrind ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -ansic
+  ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -nac
+  ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -ansic
 done
 for muls in "-255" "-111" "-43" "-3" "-2" "-1" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "23" "37" "43" "111" "255"
 do
-  valgrind ./kmul${EXE} -mul ${muls} -width 32 -signed -nac
-  valgrind ./kmul${EXE} -mul ${muls} -width 32 -signed -ansic
+  ./kmul${EXE} -mul ${muls} -width 32 -signed -nac
+  ./kmul${EXE} -mul ${muls} -width 32 -signed -ansic
 done
 
 if [ "$SECONDS" -eq 1 ]
