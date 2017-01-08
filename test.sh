1af64763a3be86a86e7910d3e2c28d459562c4e7
Nikolaos Kavvadias <kavvadias@silexica.com>
2017-01-08 15:17:14 +0100

    No need for the extra dash

--- test.sh
+++ test.sh
@@ -5,13 +5,13 @@ EXE=.exe
 # Test unsigned and signed multiplications
 for mulu in "0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "23" "37" "43" "111" "255"
 do
-  ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -nac
-  ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -ansic
+  valgrind ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -nac
+  valgrind ./kmul${EXE} -mul ${mulu} -width 32 -unsigned -ansic
 done
 for muls in "-255" "-111" "-43" "-3" "-2" "-1" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "23" "37" "43" "111" "255"
 do
-  ./kmul${EXE} -mul -${muls} -width 32 -signed -nac
-  ./kmul${EXE} -mul -${muls} -width 32 -signed -ansic
+  valgrind ./kmul${EXE} -mul ${muls} -width 32 -signed -nac
+  valgrind ./kmul${EXE} -mul ${muls} -width 32 -signed -ansic
 done
 
 if [ "$SECONDS" -eq 1 ]
